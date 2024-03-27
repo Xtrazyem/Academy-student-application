291cf2ed15e957a2f9b8568f75694c8c987c6c84
[Crazyem] <93625165+Xtrazyem@users.noreply.github.com>
2024-03-27 21:49:36 +0200

    SpringCleaning
    Cleaned TaskController and TaskServicelmpl

--- src/main/java/com/swedbank/StudentApplication/task/TaskController.java
+++ src/main/java/com/swedbank/StudentApplication/task/TaskController.java
@@ -3,12 +3,18 @@ package com.swedbank.StudentApplication.task;
 import com.swedbank.StudentApplication.group.Group;
 import com.swedbank.StudentApplication.group.GroupService;
 import com.swedbank.StudentApplication.task.exceptiion.TaskNotFoundException;
-import com.swedbank.StudentApplication.group.exception.GroupNotFoundException;
-import jakarta.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -22,25 +28,26 @@ public class TaskController {
 
     @Autowired
     public TaskController(TaskService service, GroupService groupService) {
-
         this.service = service;
         this.groupService = groupService;
-
     }
 
+    //    @GetMapping(produces = "application/json")
+//    ResponseEntity<List<Task>> findAll() {
+//        List<Task> list = service.findAll();
+//        return new ResponseEntity<>(list, HttpStatus.OK);
+//    }
     @GetMapping(produces = "application/json")
-    ResponseEntity<List<Task>> findAll() {
-        List<Task> list = service.findAll();
-        return new ResponseEntity<>(list, HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    List<Task> findAllAll() {
+        return service.findAll();
     }
-
     @GetMapping("id")
-    ResponseEntity<Task> findById(@PathVariable("id") long id)
+    @ResponseStatus(HttpStatus.OK)
+    Task findById(@PathVariable("id") long id)
             throws TaskNotFoundException {
-        Task task = service.findById(id);
-        return new ResponseEntity<>(task, HttpStatus.OK);
+        return service.findById(id);
     }
-
     @PostMapping()
     ResponseEntity<Task> saveTask(@RequestBody Task task) {
         if (task.getGroupId() != null) {
@@ -48,13 +55,13 @@ public class TaskController {
             task.setGroup(group);
         }
         Task saveTask = service.save(task);
-        return new ResponseEntity<>(saveTask, HttpStatus.OK);
+        return new ResponseEntity<>(saveTask, HttpStatus.CREATED);
     }
 
     @DeleteMapping("id")
-    public ResponseEntity<Void> deleteTask(@PathVariable("id") Long id)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void deleteTask(@PathVariable("id") Long id)
             throws TaskNotFoundException {
         service.delete(id);
-        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/swedbank/StudentApplication/task/TaskServiceImpl.java
+++ src/main/java/com/swedbank/StudentApplication/task/TaskServiceImpl.java
@@ -1,22 +1,17 @@
 package com.swedbank.StudentApplication.task;
 
-import com.swedbank.StudentApplication.config.DateTimeConfig;
-import com.swedbank.StudentApplication.person.Person;
-import com.swedbank.StudentApplication.person.exception.PersonNotFoundException;
 import com.swedbank.StudentApplication.task.exceptiion.TaskExistsException;
 import com.swedbank.StudentApplication.task.exceptiion.TaskNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class TaskServiceImpl implements  TaskService{
-    private DateTimeConfig dateformatter;
+public class TaskServiceImpl implements TaskService {
 
-    private TaskRepository repository;
+    private final TaskRepository repository;
 
     @Autowired
     public TaskServiceImpl(TaskRepository repository) {
@@ -25,15 +20,13 @@ public class TaskServiceImpl implements  TaskService{
 
     @Override
     public List<Task> findAll() {
-        List<Task> tasks = repository.findAll();
-        return tasks;
+        return repository.findAll();
     }
 
     @Override
     public Task findById(long id) throws TaskNotFoundException {
-        Task task = repository.findById(id)
-                .orElseThrow(()-> new TaskNotFoundException(id));
-        return task;
+        return repository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException(id));
     }
 
     @Override
@@ -43,25 +36,24 @@ public class TaskServiceImpl implements  TaskService{
 
     @Override
     public void update(Task task) throws TaskNotFoundException {
-    Task exsistingTask = repository.findById(task.getId())
-            .orElseThrow(() -> new TaskNotFoundException(task.getId()));
-    exsistingTask.setId(task.getId());
-    exsistingTask.setShortDesc(task.getShortDesc());
-    exsistingTask.setDetails(task.getDetails());
-    exsistingTask.setStartDate(task.getStartDate());
-    exsistingTask.setEndDate(task.getEndDate());
-    Task updatedTask = repository.save(exsistingTask);
+        Task exsistingTask = repository.findById(task.getId())
+                .orElseThrow(() -> new TaskNotFoundException(task.getId()));
+        exsistingTask.setId(task.getId());
+        exsistingTask.setShortDesc(task.getShortDesc());
+        exsistingTask.setDetails(task.getDetails());
+        exsistingTask.setStartDate(task.getStartDate());
+        exsistingTask.setEndDate(task.getEndDate());
+        repository.save(exsistingTask);
     }
 
     @Override
     public void delete(long id) throws TaskNotFoundException {
-            Optional<Task> task = repository.findById(id);
-            if (task.isPresent()){
-                repository.deleteById(id);
-            }
-            else {
-                throw new TaskNotFoundException(id);
-            }
+        Optional<Task> task = repository.findById(id);
+        if (task.isPresent()) {
+            repository.deleteById(id);
+        } else {
+            throw new TaskNotFoundException(id);
+        }
     }
 
     @Override
